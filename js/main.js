10c1d2072b94d5ddf304b43bed845f92bcab96e2
Sinakhokonke S'phelele Msibi <ST10508054@rcconnect.edu.za>
2026-09-18 21:58:39 +0200

    Added responsive CSS and JavaScript functionality-Part 2
    Added media queries for mobile and tablet responsiveness.
    Added JavaScript for hamburger menu and form validationn.
    Updated color palette and typography for T.C Carpentry.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// to check the page loaded properly. Right-click the page,
+// More will be added here in Part 3.
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("T.C. Carpentry website loaded.");
+});
